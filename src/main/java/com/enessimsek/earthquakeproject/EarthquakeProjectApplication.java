1f42420f2e49962569f8d255e6a0025f45789e01
enessimsek01 <enesimsek01@gmail.com>
2022-04-10 13:06:21 +0300

    first commit

---
+++ src/main/java/com/enessimsek/earthquakeproject/EarthquakeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.enessimsek.earthquakeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthquakeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthquakeProjectApplication.class, args);
+	}
+
+}
